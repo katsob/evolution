69f1fb14a5010f5471ba3bae5062449d52c03c17
katsob <wskf.91@gmail.com>
2019-04-11 22:10:48 +0200

    robot + animation function

---
+++ animate.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+
+# # install.packages('devtools')
+# devtools::install_github('thomasp85/gganimate')
+library(ggplot2)
+library(gganimate)
+library(reshape2)
+library(dplyr)
+
+args = commandArgs(trailingOnly=TRUE)
+evolution_dir <- args[1]
+epoch_nr <- args[2]
+
+# evolution_dir <- '2019-04-11T19:50:07'
+# epoch_nr <- '1'
+
+anim_dir <- sprintf('%s/animations', evolution_dir)
+board <- read.csv(file.path(evolution_dir, sprintf('epoch%s_board.csv', epoch_nr)), header=TRUE)
+path <- read.csv(file.path(evolution_dir, sprintf('epoch%s_path.csv', epoch_nr)), header=FALSE)
+if(!dir.exists(anim_dir))
+    dir.create(anim_dir)
+
+board$can <- factor(board$can)
+path <- cbind(path, step=seq(0, nrow(path)-1))
+colnames(path) <- c('x','y', 'step')
+class(path$step)
+ggplot() + 
+    geom_tile(data=board, aes(x = x, y = y, fill = can)) +
+    scale_fill_manual(values = c('#f4cc70', '#de7a22')) +
+    geom_point(data=path, aes(x = x, y = y), color='#20948b', size=12, shape=18) +
+    labs(x='', y='', title='Step: {frame_time}') +
+    theme(legend.position = 'none', 
+          axis.ticks=element_blank(), 
+          axis.text.x = element_blank(),
+          axis.text.y = element_blank())  +
+    transition_time(step) +
+    ease_aes('linear', interval=.001) -> anim
+anim
+
+anim_save(sprintf('%s/epoch%s.gif', anim_dir, epoch_nr), anim)
+
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import random
+import pandas as pd
 
 
 class Board:
@@ -16,14 +18,95 @@ class Board:
             return self.board[i,j]
         else:
             return -1
-        
-    def reshape(self, i):
-        b = pd.DataFrame(self.board).unstack()
-        b = b.reset_index()
-        b.columns = ['x','y','cans']
-        b.index = [i] * b.shape[0]
-        return b
     
     def __repr__(self):
         return str(self.board)
     
+    
+class RobotEngine: 
+    def __init__(self):
+        self.chromosome = []
+        
+    def L(self):
+        return self.x, self.y-1
+    
+    def D(self):
+        return  self.x+1, self.y
+    
+    def P(self):
+        return self.x, self.y
+        
+    def U(self):
+        return self.x-1, self.y
+    
+    def R(self):
+        return self.x, self.y+1
+    
+    def screen(self):
+        g = {}
+        g['L'] = self.board.loc(self.L())
+        g['D'] = self.board.loc(self.D())
+        g['P'] = self.board.loc(self.P())
+        g['U'] = self.board.loc(self.U())
+        g['R'] = self.board.loc(self.R())
+        return g
+    
+    def action(self, a='P'):
+        if a == 'P':
+            val = self.board.loc((self.x, self.y))
+            self.bag += max(0, val)
+            if val > 0:
+                self.board.board[self.x, self.y] = 0
+        else:
+            self.x, self.y = eval('self.%s()' % a)
+    
+    def move(self):
+        d = self.decision()
+        self.action(d)
+        
+    def run(self, init_board, N=50):
+        self.n = init_board.n - 1
+        self.x, self.y = [ np.random.randint(self.n) for i in range(2) ]
+        self.board_states = [init_board.board.copy()]
+        self.board = init_board
+        self.bag = 0
+        self.path = [(self.x, self.y)]
+          
+        for i in range(N):
+            self.move()
+            self.path.append((self.x, self.y))
+            bs = self.board.board.copy()
+            self.board_states.append(bs)
+        return self.bag            
+    
+    
+    def reshape(self, board, i):
+        b = pd.DataFrame(board).unstack()
+        b = b.reset_index()
+        b.columns = ['y','x','cans']
+        b.index = [i] * b.shape[0]
+        return b
+
+    def save(self, filename):
+        np.savetxt(filename + '_path.csv', np.array(self.path), delimiter=",", fmt = '%.1f')
+        to_save = []
+        for i,br in enumerate(self.board_states):
+            to_save.append(self.reshape(br, i))
+        to_save = pd.concat(to_save, axis=0)
+        to_save.index.name = 'step'
+        to_save.to_csv(filename + '_board.csv', header=True)
+    
+class RandomRobot(RobotEngine):
+    
+    def choose_direction(self, possibilities):
+        omit_wall = [ k for k,v in possibilities.iteritems() if not v == -1]
+        return np.random.choice(omit_wall)
+    
+    def decision(self):
+        possibilities = self.screen()
+        if possibilities.values() not in [ gene for gene,d in self.chromosome ]:
+            decision = self.choose_direction(possibilities)
+            self.chromosome += zip([possibilities.values()],decision)
+        else:
+            decision = [ d for gene,d in self.chromosome if gene == possibilities.values() ][0]
+        return decision
